4fc68f17c2d8f4546dc6f897e401c1513ef58e1a
James Mythen <Jammyth6@gmail.com>
2025-05-03 00:47:48 +0100

    Revert "Further updated and added a testing class"
    This reverts commit 02cd5f3054798a86eb9f7334734adc4d719236ec.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace DungeonExplorer
         {
             try
             {
-                GameTest.RunTests(); // Run the tests
                 DungeonExplorer.Game game = new DungeonExplorer.Game(); // creates a game object
 
                 // Create a Player and Inventory object
